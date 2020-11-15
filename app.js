839779f5eac74fa338d96aa129db599383fa2c9f
Riley Lai <dakimi07@gmail.com>
2020-11-15 18:52:10 +0800

    feat: add update function

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+//// 載入套件和模組
 const express = require('express')
 const exphbs = require('express-handlebars')
 const mongoose = require('mongoose')
@@ -5,11 +6,11 @@ const bodyParser = require('body-parser')
 
 const Todo = require('./models/todo')
 
+//// 設定變數
+const app = express()
 const port = 3000
 
-const app = express()
-
-// 與資料庫連線
+//// 與資料庫連線
 mongoose.connect('mongodb://localhost/todo-list', {
   useNewUrlParser: true,
   useUnifiedTopology: true
@@ -25,7 +26,7 @@ db.once('open', () => {
   console.log(`${new Date().getHours()}:${new Date().getMinutes()} MongoDB connected!`)
 })
 
-// 設定樣板引擎
+//// 設定樣板引擎
 app.engine('hbs', exphbs({
   defaultLayout: 'main',
   extname: '.hbs'
@@ -33,10 +34,10 @@ app.engine('hbs', exphbs({
 
 app.set('view engine', 'hbs')
 
-// 指定經過body-parser解析
+//// 指定經過body-parser解析
 app.use(bodyParser.urlencoded({ extended: true }))
 
-// 設定路由
+//// 設定路由
 // 首頁
 app.get('/', (req, res) => {
   Todo.find()
@@ -67,7 +68,29 @@ app.get('/todos/:id', (req, res) => {
     .catch(error => console.error(error))
 })
 
-// 啟動應用程式伺服器
+// 進入編輯頁面按鈕
+app.get('/todos/:id/edit', (req, res) => {
+  const id = req.params.id
+  Todo.findById(id)
+    .lean()
+    .then(todo => res.render('edit', { todo }))
+    .catch(error => console.error(error))
+})
+
+// 送出編輯結果按鈕
+app.post('/todos/:id/edit', (req, res) => {
+  const id = req.params.id
+  const name = req.body.name
+  Todo.findById(id)
+    .then(todo => {
+      todo.name = name
+      todo.save()
+    })
+    .then(() => res.redirect(`/todos/${id}`))
+    .catch(error => console.error(error))
+})
+
+//// 啟動應用程式伺服器
 app.listen(port, () => {
   console.log(`${new Date().getHours()}:${new Date().getMinutes()} app is running on http://localhost:${port}.`)
 })
