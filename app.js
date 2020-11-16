492902813b99fe628b7799bc04d2ccae0e9ff540
Riley Lai <dakimi07@gmail.com>
2020-11-16 20:12:18 +0800

    feat: add check funtion
    - add isDone to Todo model.
    - add isDone params to app.js.
    - add checkbox to edit, index and detail page.

--- app.js
+++ app.js
@@ -80,10 +80,13 @@ app.get('/todos/:id/edit', (req, res) => {
 // 送出編輯結果按鈕
 app.post('/todos/:id/edit', (req, res) => {
   const id = req.params.id
-  const name = req.body.name
+  const { name, isDone } = req.body
+  console.log(name)
+  console.log(isDone)
   Todo.findById(id)
     .then(todo => {
       todo.name = name
+      todo.isDone = isDone === 'on'
       todo.save()
     })
     .then(() => res.redirect(`/todos/${id}`))
